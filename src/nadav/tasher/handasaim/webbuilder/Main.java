2b29e8f864ba8d6052f98d7b208e9ab125433aa3
nadav <tashernadav@gmail.com>
2018-09-01 07:41:16 -0400

    Version 0.1 Code Revision 02

--- src/nadav/tasher/handasaim/webbuilder/Main.java
+++ src/nadav/tasher/handasaim/webbuilder/Main.java
@@ -1,8 +1,12 @@
 package nadav.tasher.handasaim.webbuilder;
 
+import org.json.JSONObject;
+
 public class Main {
+    private JSONObject result = new JSONObject();
 
     public static void main(String[] args) {
+        System.out.println(args);
         System.out.println("Success");
     }
 }
